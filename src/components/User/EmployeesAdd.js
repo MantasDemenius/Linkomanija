e2342ff31ac39dc3cdbb2892fc984ef2dc6b2ec9
Mantas <mantas.demenius@gmail.com>
2019-12-11 16:36:33 +0200

    Registration for basic user and for employee

--- src/components/User/EmployeesAdd.js
+++ src/components/User/EmployeesAdd.js
@@ -1,14 +1,16 @@
-import React from "react";
-import '../../App.css'
-import {Form, Card} from 'antd';
+import React from 'react';
+import '../../App.css';
+import { Form, Card } from 'antd';
 import RegistrationForm from '../Registration/RegistrationForm';
 
 const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
 
 const EmployeesAdd = () => {
-  return (<Card title="Darbuotojo registracija">
-    
-    <WrappedRegistrationForm/></Card>)
-}
+  return (
+    <Card title="Darbuotojo registracija">
+      <WrappedRegistrationForm userType="employee" />
+    </Card>
+  );
+};
 
-  export default EmployeesAdd;
+export default EmployeesAdd;
