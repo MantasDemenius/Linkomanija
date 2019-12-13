f0ee4fb83005a11e1c1f9d94ff7cfc68328c9a39
Mantas <mantas.demenius@gmail.com>
2019-12-13 17:23:37 +0200

    new route

--- src/common/Routes.js
+++ src/common/Routes.js
@@ -1,3 +1,4 @@
 export const ADD_CLIENT_ROUTE = 'users/clients';
 export const ADD_EMPLOYEE_ROUTE = 'users/employee/add';
 export const ADD_MOVIEHALL_ROUTE = 'movieHall/add';
+export const GET_CLIENTS_EMPLOYEES = 'users/';
