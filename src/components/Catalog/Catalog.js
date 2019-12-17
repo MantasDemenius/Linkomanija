1e2966c717facafa0b798126dddac0fe2c567cf8
matas <matasspeicys@gmail.com>
2019-12-17 17:47:00 +0200

    filter movies

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -12,6 +12,9 @@ import axios from 'axios';
 const Catalog = () => {
     let history = useHistory();
     const [movies, setMovies] = useState([]);
+    const [selectedLanguages, setSelectedLanguages] = useState([]);
+    const [selectedCensorship, setSelectedCensorship] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState([]);
 
     useEffect(() => {
         axios.get('/api/movie')
@@ -24,7 +27,32 @@ const Catalog = () => {
     }, []);
     console.log(movies);
 
-    const movieCards = movies.map(movie => {
+    const onChangeLanguage = (checkedValues) => {
+        setSelectedLanguages(checkedValues)
+    };
+
+    const onChangeCensorhip = (checkedValues) => {
+        setSelectedCensorship(checkedValues)
+    };
+
+    const onChangeGenre = (checkedValues) => {
+        setSelectedGenres(checkedValues)
+    };
+
+    const movieFilter = (movie) => {
+        let languagePass = true;
+        let censorshipPass = true;
+        let genresPass = true;
+        if (selectedLanguages.length > 0)
+            languagePass = selectedLanguages.includes(movie.language.id);
+        if (selectedCensorship.length > 0)
+            censorshipPass = selectedCensorship.includes(movie.age_censor);
+        if (selectedGenres.length > 0)
+            genresPass = selectedGenres.some(genre => movie.genreList.map(genre => genre.id).includes(genre));
+        return languagePass && censorshipPass && genresPass;
+    };
+
+    const movieCards = movies.filter(movieFilter).map(movie => {
         return (
             <MovieCard
                 key={movie.id}
@@ -37,7 +65,7 @@ const Catalog = () => {
         <>
             <Row>
                 <Col span={6}>
-                    <Filter />
+                    <Filter onChangeLanguage={onChangeLanguage} onChangeCensorhip={onChangeCensorhip} onChangeGenre={onChangeGenre} />
                 </Col>
                 <Col span={18}>
                     <Sorter />
