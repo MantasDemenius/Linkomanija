35361c8f06336027db073f2fdd354d48648a8ef6
matas <matasspeicys@gmail.com>
2019-12-18 09:12:40 +0200

    fix mail

--- backend/src/main/java/com/linkomanija/backend/controller/ReservationController.java
+++ backend/src/main/java/com/linkomanija/backend/controller/ReservationController.java
@@ -1,11 +1,16 @@
 package com.linkomanija.backend.controller;
 
+import com.itextpdf.text.DocumentException;
 import com.linkomanija.backend.domain.Reservation;
 import com.linkomanija.backend.dto.ReservationDTO;
+import com.linkomanija.backend.mail.Mail;
+import com.linkomanija.backend.pdf.PdfGenerator;
 import com.linkomanija.backend.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin
@@ -16,9 +21,21 @@ public class ReservationController {
   @Autowired
   private ReservationService reservationService;
 
+  @Autowired
+  private PdfGenerator pdfGenerator;
+
+  @Autowired
+  private Mail mail;
+
   @PostMapping(consumes = "application/json", produces = "application/json")
-  public Reservation addReservation(@RequestBody ReservationDTO reservationDTO) {
-    return reservationService.addReservation(reservationDTO);
+  public Reservation addReservation(@RequestBody ReservationDTO reservationDTO) throws IOException, DocumentException, MessagingException {
+    Reservation reservation = reservationService.addReservation(reservationDTO);
+    if (reservation.isTicket_state()) {
+      pdfGenerator.generateDocument(reservation, "invoice");
+      pdfGenerator.generateDocument(reservation, "ticket");
+      mail.sendDocuments(reservation);
+    }
+    return reservation;
   }
 
   @PostMapping(value = "change-seat", produces = "application/json")
